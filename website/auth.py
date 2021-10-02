240cc4b6d8edc4c7c238af5969fe691ffe8aec0a
Oren Zion <orenzion89@gmail.com>
2021-10-02 23:25:18 +0200

    defined auth routes

--- website/auth.py
+++ website/auth.py
@@ -1,3 +1,18 @@
 from flask import Blueprint
 
 auth = Blueprint('auth', __name__)
+
+
+@auth.route('/Login')
+def login():
+    return "<p>Login</p>"
+
+
+@auth.route('/Logout')
+def logout():
+    return "<p>Logout</p>"
+
+
+@auth.route('/sign-up')
+def sign_up():
+    return "<p>Sign Up</p>"
