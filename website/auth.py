5314c560a3f7434bd91ff92461679b07e11289f0
Oren Zion <orenzion89@gmail.com>
2021-10-05 22:23:31 +0200

    create new user and add t0 db

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,8 @@
 from flask import Blueprint, render_template, request, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+
+from .models import User
 
 auth = Blueprint('auth', __name__)
 
@@ -30,6 +34,14 @@ def sign_up():
         elif len(password1) < 7:
             flash('password must be at least 7 characters.', category='error')
         else:
-            flash('account created.', category='success')
+            # create new user
+            new_user = User(email=email, firstName=firstName, password=generate_password_hash(
+                password1, method='sha256'))
+            # add new user to db
+            db.session.add(new_user)
+            # commit transaction
+            db.session.commit()
+            # notify user with flash message
+            flash('Account Created.', category='success')
 
     return render_template("sign_up.jinja")
